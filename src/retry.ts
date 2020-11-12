a54d24f5b64afd133f83244ea3293bc0c399e7d4
Andrius Mozūraitis <andrius.mozuraitis@searchnode.net>
2020-11-12 09:00:30 +0200

    Added retry on fail helper

---
+++ src/retry.ts
@@ -0,0 +1,92 @@
+
+
+// args?: {
+//     retryCount?: number
+//     retryCondition?: (err: Error) => boolean
+//     delay?: number | ((count: number) => Promise<void>)
+// }
+
+import { delay } from "./delay"
+import { ensureError } from "./errors"
+import { isNumber } from "./guards"
+
+export interface RetryArgs {
+    count: number,
+    err: Error
+    startTime: number
+    logger?: Partial<Pick<Console, 'warn'>>
+}
+export const RETRY_STRATEGIES = {
+    EXP: (options: {
+        retryOnError: (err: Error) => boolean
+        retryCount: number
+        retryDelay: number | ((count: number) => number)
+    }) => {
+        const { retryCount, retryDelay, retryOnError } = options
+        return async (args: RetryArgs) => {
+            isNumber(retryDelay)
+            const delayMs = isNumber(retryDelay) ? retryDelay : retryDelay(args.count)
+            const shouldRetry = () => {
+                if (retryCount < args.count) return {
+                    ...args,
+                    retry: false
+                }
+                if (retryOnError(args.err)) return {
+                    ...args,
+                    retry: true
+                }
+                return true
+            }
+            if (!shouldRetry()) return {
+                ...args,
+                retry: false
+            }
+
+            args.logger?.warn?.(`Retrying ${args.count}. Error ${args.err.message}`)
+            await delay(delayMs)
+            return {
+                ...args,
+                count: args.count + 1,
+                retry: true
+            }
+        }
+    }
+}
+export type RetryFn = (args: RetryArgs) => Promise<RetryArgs & { retry: boolean }>
+
+const defaultRetryStrategy = RETRY_STRATEGIES.EXP({ retryCount: 5, retryDelay: (count) => Math.pow(count, 2) * 1000, retryOnError: () => true })
+
+/**
+ * Retry promise on exception
+ * @param fn - function that return promise
+ * @param options - Options
+ */
+export interface RetryOnFailArgs {
+    /**
+     * Check `RETRY_STRATEGIES` for available retry strategies
+     */
+    retryStrategy?: RetryFn
+    /**
+     * Logging function you can use regular console object or custom logger
+     */
+    logger?: Pick<Console, 'warn'>
+}
+export function retryOnFail<I extends any[], R>(fn: (...args: I) => Promise<R>, options?: RetryOnFailArgs): (...args: I) => Promise<R> {
+    const { retryStrategy: retryFn = defaultRetryStrategy } = options || {}
+    return (...args: I) => {
+        const startTime = Date.now()
+        return fn(...args).catch(async (e) => {
+            const err = ensureError(e)
+            let prev: RetryArgs | undefined = undefined
+            while (true) {
+                const retryFDS: RetryArgs & { retry: boolean } = await retryFn(prev || { count: 0, err, startTime, logger: options?.logger })
+                if (retryFDS.retry) {
+                    prev = { ...retryFDS as any }
+                    continue
+                }
+                throw err
+            }
+
+        })
+    }
+}
